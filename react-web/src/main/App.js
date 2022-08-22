8169704af551f51822e50a10f2191c5d2f53031f
Ajith <ajithunair@outlook.com>
2022-08-22 13:33:17 +0530

    intial commit

---
+++ react-web/src/main/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="Container">
+      <Header subtitle='Providing houses all over the world' />
+    </div>
+  );
+}
+
+export default App;
